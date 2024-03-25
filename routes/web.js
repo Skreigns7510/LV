5e1ab22f8f420a88ae8a5e67600620930e2398f5
Skreigns7510 <saksham.ff.garena@gmail.com>
2024-03-25 23:45:06 +0530

    caraousel

--- routes/web.js
+++ routes/web.js
@@ -3,6 +3,7 @@ const FrontController = require('../Controllers/FrontController')
 const route = express.Router()
 
 route.get('/',FrontController.home)
+route.get('/home.ejs',FrontController.home)
 route.get('/contact.ejs',FrontController.contact)
 
 
